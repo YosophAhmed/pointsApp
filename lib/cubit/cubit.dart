afd670c009fe0ef3a38a4f041b8b309a870f29ba
YosophAhmed <devyosoph@gmail.com>
2022-11-11 19:00:16 +0200

    Cubit

---
+++ lib/cubit/cubit.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:points/cubit/states.dart';
+
+class PointsCubit extends Cubit<PointsStates> {
+  PointsCubit(super.initialState);
+
+}
---
+++ lib/cubit/states.dart
@@ -0,0 +1,10 @@
+
+abstract class PointsStates{}
+
+class TeamAPointsState extends PointsStates{
+
+}
+
+class TeamBPointsState extends PointsStates {
+
+}
